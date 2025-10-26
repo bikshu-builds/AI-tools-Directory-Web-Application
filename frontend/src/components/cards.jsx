dadca5a104f788f768810ed731d8268741f02429
bikshu-builds <bikshutulimelli@gmail.com>
2025-10-26 20:35:41 +0530

    Update cards.jsx

--- frontend/src/components/cards.jsx
+++ frontend/src/components/cards.jsx
@@ -32,7 +32,7 @@ function Cards() {
 
     const handleCardClick = async (categoryName) => {
         try {
-            const res = await axios.get(`https://ai-tools-directory-web-application.onrender.com/get-data/${categoryName}`);
+            const res = await axios.get(`https://ai-tools-directory-web.onrender.com/get-data/${categoryName}`);
             setData(res.data);
             setCategory(categoryName);
             navigate('/cat');
