c8d39e842967f72260402260d232246a5c71cc32
bikshu-builds <bikshutulimelli@gmail.com>
2025-10-26 19:42:07 +0530

    Update comparetools.jsx

--- frontend/src/components/comparetools.jsx
+++ frontend/src/components/comparetools.jsx
@@ -18,14 +18,14 @@ function CompareTools() {
   const [toolData, setToolData] = useState([]);
 
   const fetchTools = (category, setTools) => {
-    axios.get(`http://localhost:5000/tools/${category}`)
+    axios.get(`https://ai-tools-directory-web-application.onrender.com/tools/${category}`)
       .then(res => setTools(res.data))
       .catch(err => console.error(err));
   };
 
   const handleCompare = () => {
     if (selectedTool1 && selectedTool2 && selectedCategory1 && selectedCategory2) {
-      axios.get('http://localhost:5000/compare', {
+      axios.get('https://ai-tools-directory-web-application.onrender.com/compare', {
         params: {
           tool1: selectedTool1,
           tool2: selectedTool2,
